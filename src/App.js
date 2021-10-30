8193df166aed8487e74c429a3d61f913cbb8a4a2
Mohamed Abduljawad <salintofficial@hotmail.com>
2021-10-30 22:11:48 +0200

    Save user in context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // Dependencies, lol
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "./services/FirebaseService";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // Pages
@@ -9,17 +10,40 @@ import Login from "./components/pages/Login";
 
 // Cool styling ;)
 import "./style.css";
+import UserContext from "./context/UserContext";
 
-const App = () => (
-	<React.StrictMode>
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route exact path="/signup" component={Signup} />
-				<Route exact path="/login" component={Login} />
-			</Switch>
-		</BrowserRouter>
-	</React.StrictMode>
-);
+const App = () => {
+
+	const [ pending, setPending ] = useState(true);
+	const [ user, setUser ] = useState(undefined);
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged((user) => {
+			if(user) {
+				setUser(user);
+			}
+			setPending(false);
+
+		});
+	});
+
+	if(pending) {
+		return <></>;
+	}
+
+	return (
+		<UserContext.Provider value={user}>
+			<React.StrictMode>
+				<BrowserRouter>
+					<Switch>
+						<Route exact path="/" component={HomePage} />
+						<Route exact path="/signup" component={Signup} />
+						<Route exact path="/login" component={Login} />
+					</Switch>
+				</BrowserRouter>
+			</React.StrictMode>
+		</UserContext.Provider>
+	);
+};
 
 export default App;
