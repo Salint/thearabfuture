edcb55d1e14f4e1c11a270c375979672e946d4b8
Mohamed Abduljawad <salintofficial@hotmail.com>
2022-01-06 10:32:14 +0200

    Added basic pages for questions

---
+++ src/components/pages/Question.js
@@ -0,0 +1,61 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import QuestionService from "../../services/QuestionService";
+
+import QuestionPageTemplate from "../templates/QuestionPageTemplate";
+
+
+const Question = ({ match }) => {
+	
+	const questionService = new QuestionService();
+
+	const [ question, setQuestion ] = useState();
+	const [ pending, setPending ] = useState(true);
+	const [ error, setError ] = useState("");
+
+	useEffect(() => {
+
+		(async () => {
+
+			setPending(true);
+			try {
+				const tempQuestion = await questionService.getQuestion(match.params.id);
+				
+				setQuestion(tempQuestion);
+				setError("");
+				setPending(false);
+			}
+			catch(error) {
+				setError(error.code);
+				setPending(false);
+			}
+
+		})();
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [match.params.id]);
+	
+	if(pending) {
+		return <QuestionPageTemplate pending={true} />;
+	}
+	
+	if(!match.params.id) {
+		return <Redirect to="/questions" />;
+	}
+	
+	
+	if(error.length > 0) {
+		return <QuestionPageTemplate error={error} />;
+	}
+	
+	return (
+		<>
+			<QuestionPageTemplate 
+				question={question}
+			/>
+		</>
+	)
+
+};
+
+export default Question;
