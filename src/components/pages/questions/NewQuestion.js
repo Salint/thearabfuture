d28c1e507d6ae5f0d860bf9087776612a400d852
Mohamed Abduljawad <salintofficial@hotmail.com>
2022-01-11 12:46:14 +0200

    Only allow logged in users to ask

--- src/components/pages/questions/NewQuestion.js
+++ src/components/pages/questions/NewQuestion.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+import { AuthProvider, IfFirebaseAuthed, IfFirebaseUnAuthed } from "../../../context/FirebaseAuthContext";
 
 import PageTemplate from "../../templates/PageTemplate";
 
 import NewQuestionForm from "../../organisms/questions/NewQuestionForm";
 
 const NewQuestion = () => (
-	<PageTemplate>
-		<NewQuestionForm />
-	</PageTemplate>
+	<AuthProvider>
+		<IfFirebaseUnAuthed>
+			<Redirect to="/login" />
+		</IfFirebaseUnAuthed>
+		<IfFirebaseAuthed>
+			<PageTemplate>
+				<NewQuestionForm />
+			</PageTemplate>
+		</IfFirebaseAuthed>
+	</AuthProvider>
+
 );
 
 export default NewQuestion;
