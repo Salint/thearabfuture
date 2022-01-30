443d92778661aec65fe645971e83e3f214c40536
Mohamed Abduljawad <salintofficial@hotmail.com>
2022-01-30 13:12:20 +0200

    Import analytics

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -2,6 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import "firebase/compat/analytics"
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDfqoU3E3bTM0vB3yPJ9VlyQodCudaJmms",
