46e91c103355d41047f872f337c33c567b6a3239
Mohamed Abduljawad <salintofficial@hotmail.com>
2022-02-05 14:15:11 +0200

    Store author in articles

--- src/services/ArticleService.js
+++ src/services/ArticleService.js
@@ -4,62 +4,55 @@ import UserService from "./UserService";
 class ArticleService {
 
 	async fetchArticles() {
-
-		const userService = new UserService();
-
+		
+		
 		try {
-
+			
 			let articles = [];
 			
 			const articleData = await firebase.firestore().collection("articles").orderBy("date", "desc").get();
-
+			
 			for (const article of articleData.docs) {
-
-				const author = await userService.fetchUser(article.get("author"));
-
+				
 				articles.push({
 					id: article.id,
 					title: article.get("title"),
 					content: article.get("content"),
 					category: article.get("category"),
 					date: article.get("date").toDate(),
-					userData: author
+					userData: article.get("author")
 				});
-
+				
 			}
-
+			
 			return articles;
 		}	
 		catch(error) {
 			throw new Error("حدث خطأ");
 		}
 	}
-
+	
 	async getArticle(id) {
-
-		const userService = new UserService();
-
+		
 		try {
 			
 			const article = await firebase.firestore().collection("articles").doc(id).get();
-
+			
 			if(article.exists) {
-	
-				const author = await userService.fetchUser(article.get("author"));
-
+				
 				return {
 					id: article.id,
 					title: article.get("title"),
 					content: article.get("content"),
 					category: article.get("category"),
 					date: article.get("date").toDate(),
-					userData: author
+					userData: article.get("author")
 				};
 			}
 			else {
 				const error = new Error();
 				error.code = "article/article-not-found";
-
+				
 				throw error;
 			}
 		}	
@@ -67,12 +60,20 @@ class ArticleService {
 			throw error;
 		}
 	}
-
+	
 	async addArticle(uid, title, category, content) {
+		
+		const userService = new UserService();
 
 		try {
+			const { username, profileURL } = await userService.fetchUser(uid);
+
 			await firebase.firestore().collection("articles").add({
-				author: uid,
+				author: {
+					uid: uid,
+					username: username,
+					profileURL: profileURL ? profileURL : ""
+				},
 				title: title,
 				category: category,
 				content: content,
@@ -80,6 +81,7 @@ class ArticleService {
 			});
 		}
 		catch(error) {
+			console.error(error);
 			throw new Error("حدث خطأ أثناء نشر السؤال");
 		}
 	}
