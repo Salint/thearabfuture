650bd0261a88590a7d83003fe37e7df0fa4fe940
Mohamed Abduljawad <salintofficial@hotmail.com>
2023-03-25 00:46:33 +0200

    Update username in all documents

--- functions/src/event/listeners/users/updateUserProfile.js
+++ functions/src/event/listeners/users/updateUserProfile.js
@@ -1,23 +1,45 @@
 const functions = require("firebase-functions");
 const admin = require("../../../util/admin");
 
-module.exports.updateQuestionData = functions.firestore.document("/users/{uid}").onUpdate(async (change, context) => {
-	return admin.firestore().collection("questions").where("author.uid", "==", context.params.uid).get().then(results => {
-		results.forEach(doc => {
-			const promises = [];
+const updateAuthor = (result, change) => {
+	const promises = [];
 
-			promises.push(doc.ref.update({
-				author: {
-					...doc.data().author,
-					username: change.after.data().username
-				}
-			}));
-			
-			return Promise.all(promises);
-		});
-	}).then(error => {
-		functions.logger.error(error);
+	result.forEach(doc => {
+		promises.push(doc.ref.update({
+			author: {
+				...doc.data().author,
+				username: change.after.data().username
+			}
+		}));
+	});
+
+	return promises;
+};
+
+module.exports.updateQuestionData = functions.firestore.document("/users/{uid}").onUpdate(async (change, context) => {
+	const promises = [];
+
+	const questions = admin.firestore().collection("questions").where("author.uid", "==", context.params.uid).get();
+	promises.push(questions);
+
+	const answers = admin.firestore().collectionGroup("answers").get();
+	promises.push(answers);
+
+	questions.then(result => {
+		promises.push(updateAuthor(result, change));
+
+	}).catch(error => {
+		console.log(error);
 		return null;
 	});
+	answers.then(result => {
+		promises.push(updateAuthor(result, change));
+		
+	}).catch(error => {
+		console.log(error);
+		return null;
+	});
+
+	return Promise.all(promises);
 	
 });
