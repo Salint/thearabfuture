f9f4be72af5788410e11fc8eaf014661a4c5fe4c
Mohamed Abduljawad <salintofficial@hotmail.com>
2023-06-27 17:09:24 +0300

    Fix

--- functions/src/event/listeners/users/updateUserProfile.js
+++ functions/src/event/listeners/users/updateUserProfile.js
@@ -2,18 +2,33 @@ const functions = require("firebase-functions");
 const admin = require("../../../util/admin");
 
 const updateAuthor = (result, change) => {
-	const promises = [];
+	let promise;
 
 	result.forEach(doc => {
-		promises.push(doc.ref.update({
+		promise = doc.ref.update({
 			author: {
 				...doc.data().author,
 				username: change.after.data().username
 			}
-		}));
+		});
 	});
 
-	return promises;
+	return promise;
+};
+
+const updatePicture = (result, change) => {
+	let promise;
+
+	result.forEach(doc => {
+		promise = doc.ref.update({
+			author: {
+				...doc.data().author,
+				username: change.after.data().username
+			}
+		});
+	});
+
+	return promise;
 };
 
 module.exports.updateQuestionData = functions.firestore.document("/users/{uid}").onUpdate(async (change, context) => {
@@ -22,11 +37,40 @@ module.exports.updateQuestionData = functions.firestore.document("/users/{uid}")
 	const questions = admin.firestore().collection("questions").where("author.uid", "==", context.params.uid).get();
 	promises.push(questions);
 
-	const answers = admin.firestore().collectionGroup("answers").get();
+	const answers = admin.firestore().collectionGroup("answers").where("author.uid", "==", context.params.uid).get();
 	promises.push(answers);
 
 	questions.then(result => {
 		promises.push(updateAuthor(result, change));
+
+	}).catch(error => {
+		console.log(error);
+		return null;
+	});
+	answers.then(result => {
+		promises.push(updateAuthor(result, change));
+		
+	}).catch(error => {
+		console.log(error);
+		return null;
+	});
+
+	return Promise.all(promises);
+	
+});
+module.exports.updateQuestionsPictures = functions.storage.object(). onUpdate(async (change, context) => {
+	const promises = [];
+
+	const questions = admin.firestore().collection("questions").where("author.uid", "==", context.params.uid).get();
+	promises.push(questions);
+
+	const answers = admin.firestore().collectionGroup("answers").where("author.uid", "==", context.params.uid).get();
+	promises.push(answers);
+
+	questions.then(result => {
+		result.forEach(doc => {
+			const link = await context
+		});
 
 	}).catch(error => {
 		console.log(error);
